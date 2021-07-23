6c862b085f014368e33711c1cf44429845b0a096
Ryan <60561289+qaz45647@users.noreply.github.com>
2021-07-23 22:53:03 +0800

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -1,10 +1,35 @@
 import discord
+from discord import file
 from discord.ext import commands
+import json
+import random
 
-bot = commands.Bot(command_prefix='1')
+def pic_rd(length):         #隨機生成五個字
+    prd = str()
+    characters = "qwertyuiopasdfghjklzxcvbnm" + "1234567890"
+    for i in range(length):
+        prd = prd +  random.choice(characters)
+    return prd
+
+with open('setting.json','r',encoding='utf8') as jfile:
+    jdata = json.load(jfile)
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix='[')
 
 @bot.event
 async def on_ready():
     print(">> Bot is online <<")
 
-bot.run("ODY3MDM4MTczMzg5NzgzMDcx.YPbSOQ.2QQvRhpORiAG5bSJNzKIAyNnUDA")
+
+@bot.command()            #機器人Ping
+async def ping(ctx):
+    await ctx.send(f"{round(bot.latency*1000)} ms")
+
+@bot.command()          #隨機生成一張支語警察圖片
+async def Police_IMG(ctx):    
+    await ctx.send(jdata['pic']+pic_rd(5)+'.jpg')
+
+
+bot.run(jdata['Bot_TOKEN'])
